4864bdea70101838ee59e35c306eec3fb3bd7ba4
hansaem <dosaem90@gmail.com>
2019-09-18 10:09:26 +0900

    5.2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import EventPractice from './EventPractice';
+import React, { Component } from 'react';
 import ValidationSample from './ValidationSample';
 
-const App = () => {
-  return <ValidationSample />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ValidationSample />
+      </div>
+    );
+  }
+}
 
 export default App;
